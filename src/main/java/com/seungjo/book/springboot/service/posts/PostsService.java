7b0118dfba852515237569582a13912946860072
f1v3-dev <f1v3_dev@naver.com>
2023-03-28 01:18:44 +0900

    [Mustache 활용] 게시글 수정, 삭제 페이지 생성

--- src/main/java/com/seungjo/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/seungjo/book/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.seungjo.book.springboot.service.posts;
 
 import com.seungjo.book.springboot.domain.posts.Posts;
 import com.seungjo.book.springboot.domain.posts.PostsRepository;
+import com.seungjo.book.springboot.web.dto.PostsListResponseDto;
 import com.seungjo.book.springboot.web.dto.PostsResponseDto;
 import com.seungjo.book.springboot.web.dto.PostsSaveRequestDto;
 import com.seungjo.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 
 
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,5 +40,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow( () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
