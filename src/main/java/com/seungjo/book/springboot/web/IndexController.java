eb65ca18240048bbdcfa06a212ad10ec3a8e8b4a
f1v3-dev <f1v3_dev@naver.com>
2023-05-11 17:50:33 +0900

    [배포 테스트]

--- src/main/java/com/seungjo/book/springboot/web/IndexController.java
+++ src/main/java/com/seungjo/book/springboot/web/IndexController.java
@@ -49,7 +49,10 @@ public class IndexController {
 
 
     @GetMapping("/posts/save")
-    public String postsSave(){
+    public String postsSave(Model model, @LoginUser SessionUser user){
+        if (user != null) {
+            model.addAttribute("loginUserName", user.getName());
+        }
         return "post/posts-save";
     }
 
