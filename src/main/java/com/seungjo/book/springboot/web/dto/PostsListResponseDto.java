f299c6cd474f5c604f66adf131304888f9b4aa43
f1v3-dev <f1v3_dev@naver.com>
2023-05-22 14:49:46 +0900

    Revert "[게시글 - 작성시간 포맷 변경('yyyy-mm-dd hh:mm:ss')]"
    This reverts commit 4f5a03be0097b1288ce30d54e53b12b78d5c9fca.

--- src/main/java/com/seungjo/book/springboot/web/dto/PostsListResponseDto.java
+++ src/main/java/com/seungjo/book/springboot/web/dto/PostsListResponseDto.java
@@ -10,7 +10,7 @@ public class PostsListResponseDto {
     private Long id;
     private String title;
     private String author;
-    private String modifiedDate;
+    private LocalDateTime modifiedDate;
     private String image;
 
     public PostsListResponseDto(Posts entity) {
