bdcc77abd59988487aef57336a818039bfd8917c
f1v3-dev <f1v3_dev@naver.com>
2023-05-26 11:57:44 +0900

    [시간 표시 변경 (yyyy-mm-dd HH:mm:ss)]

--- src/main/java/com/seungjo/book/springboot/web/dto/PostsListResponseDto.java
+++ src/main/java/com/seungjo/book/springboot/web/dto/PostsListResponseDto.java
@@ -10,14 +10,14 @@ public class PostsListResponseDto {
     private Long id;
     private String title;
     private String author;
-    private LocalDateTime modifiedDate;
-    private String image;
+    private String modifiedDate;
+
 
     public PostsListResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
         this.modifiedDate = entity.getModifiedDate();
-        this.image = entity.getImage();
+
     }
 }
