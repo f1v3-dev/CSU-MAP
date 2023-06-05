0f38f708caea68b929d9c0d1e9dc159290e41b78
f1v3-dev <f1v3_dev@naver.com>
2023-06-06 03:05:47 +0900

    [2023-06-06 Update (deploy, BuildingsController)]

--- src/main/java/com/seungjo/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/seungjo/book/springboot/config/auth/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile", "/login", "/introduce", "/notice", "/find", "/building/**", "/IT/**").permitAll()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile", "/login", "/introduce", "/notice", "/find", "/buildings/**").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and()
